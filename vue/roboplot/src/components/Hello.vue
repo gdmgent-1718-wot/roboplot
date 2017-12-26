e3372a6d507bc0587a282c67917e83f0c13db64d
lodemuyl <lodemuyl@student.arteveldehs.be>
2017-12-26 17:55:21 +0100

    vuefrontend

---
+++ vue/roboplot/src/components/Hello.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="main">
+    <div class="container">
+      <div class="card">
+        <h1>Opgenomen</h1>
+        <ul class="list-group">
+          <li class="list-group-item textleft">
+            <span class="badge badge-primary badge-pill">datum</span>
+            <span class="left">Dapibus ac facilisis in</span>
+            <a class="right actionbuttons"><i class="fa fa-plus fa-2x" aria-hidden="true"></i></a>
+            <a class="right actionbuttons"><i class="fa fa-minus fa-2x" aria-hidden="true"></i></a>
+          </li>
+          <li class="list-group-item textleft">
+            <span class="badge badge-primary badge-pill right">datum</span>
+            <span class="left">Dapibus ac facilisis in</span>
+            <a class="right actionbuttons"><i class="fa fa-plus fa-2x" aria-hidden="true"></i></a>
+            <a class="right actionbuttons"><i class="fa fa-minus fa-2x" aria-hidden="true"></i></a>
+          </li>
+          <li class="list-group-item textleft act">
+            <span class="badge badge-primary badge-pill right">datum</span>
+            <span class="left">Dapibus ac facilisis in</span>
+            <a class="right actionbuttons white"><i class="fa fa-minus fa-2x" aria-hidden="true"></i></a>
+          </li>
+        </ul>
+        <nav aria-label="Page navigation example">
+          <ul class="pagination justify-content-end">
+            <li class="page-item disabled">
+              <a class="page-link" href="#" tabindex="-1">Vorige</a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+              <a class="page-link" href="#">Volgende</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="card">
+        <h1>Afspeellijst</h1>
+         <ul class="list-group">
+          <li class="list-group-item textleft act">
+            <span class="badge badge-primary badge-pill right">datum</span>
+            <span class="left">Dapibus ac facilisis in</span>
+            <a class="right actionbuttons white"><i class="fa fa-minus fa-2x" aria-hidden="true"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
---
+++ vue/roboplot/src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import 'font-awesome/css/font-awesome.css'
+import '@/assets/js/main.js'
+import '@/assets/sass/main.scss'
+require('../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
