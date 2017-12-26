4f1d4c61f5c26a269dfe1af80d4a924ac2758417
Jef Roosens <jef.roosens@gmail.com>
2017-12-26 17:41:22 +0100

    add analog digital

--- driver/app.py
+++ driver/app.py
@@ -33,7 +33,6 @@ Motor2E = 17 # Set GPIO-25 as Enable pin 1 of the controller IC
 
 #lampen
 lamp1 =  5
-klakson = 6
 
 GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
@@ -44,10 +43,8 @@ GPIO.setup(Motor2B,GPIO.OUT)
 GPIO.setup(Motor2E,GPIO.OUT)
 
 GPIO.setup(lamp1, GPIO.OUT)
-GPIO.setup(klakson, GPIO.OUT)
 
 pwmA=GPIO.PWM(25,100) #confuguring Enable pin means GPIO-25 for PWM
-pwmB=GPIO.PWM(17,100) #confuguring Enable pin means GPIO-17 for PWM
 
 buttonDelay = 0
 
@@ -65,27 +62,27 @@ while active:
 		left_joystick_y = math.ceil(left_joystick_y * 100) / 100
 	left = ((left_joystick_y + 1) / 2) * 100
 	if (left < 0):
-                left = 0
+    left = 0
 	if (left == 50):
 		left = 0
 	else:
 		left = left * 2 - 100
 	
 	# Right joystick parsing of data
-        right_joystick_y = ps3.a_joystick_right_y
-        if (right_joystick_y != 0):
-                right_joystick_y = right_joystick_y * -1 
-        if (right_joystick_y < 0):
-                right_joystick_y = math.floor(right_joystick_y * 100) / 100
-        elif (right_joystick_y > 0):
-                right_joystick_y = math.ceil(right_joystick_y * 100) / 100
+	right_joystick_y = ps3.a_joystick_right_y
+	if (right_joystick_y != 0):
+		right_joystick_y = right_joystick_y * -1 
+	if (right_joystick_y < 0):
+		right_joystick_y = math.floor(right_joystick_y * 100) / 100
+	elif (right_joystick_y > 0):
+		right_joystick_y = math.ceil(right_joystick_y * 100) / 100
 	right = ((right_joystick_y + 1) / 2) * 100
 	if (right < 0):
 		right = 0
-        if (right == 50):
-                right = 0
-        else:
-                right = right * 2 - 100
+	if (right == 50):
+		right = 0
+	else:
+		right = right * 2 - 100
 
 	# Debugging
 	print "[L: " + str(left) + ", R: " + str(right) + "]"
@@ -103,18 +100,7 @@ while active:
 		pwmA.ChangeDutyCycle(left * -1)
 		GPIO.output(Motor1A,GPIO.HIGH)
 		GPIO.output(Motor1B,GPIO.LOW)
-        if (right == 0):
-                pwmB.stop()
-        elif (right > 0):
-		pwmB.start(0)
-		pwmB.ChangeDutyCycle(right)
-		GPIO.output(Motor2A,GPIO.LOW)
-		GPIO.output(Motor2B,GPIO.HIGH)
-        elif (right < 0):
-		pwmB.start(0)
-		pwmB.ChangeDutyCycle(right * -1)
-		GPIO.output(Motor2A,GPIO.HIGH)
-		GPIO.output(Motor2B,GPIO.LOW)
+	print ps3.square
 	buttonDelay += 1
 	if (ps3.a_square > 0):
 		active = False
