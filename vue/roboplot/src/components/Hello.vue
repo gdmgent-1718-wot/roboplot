adb60fecd00f38e1575ecde5ac84d7767c910a7e
lodemuyl <lodemuyl@student.arteveldehs.be>
2017-12-27 14:53:45 +0100

    firebase

--- vue/roboplot/src/components/Hello.vue
+++ vue/roboplot/src/components/Hello.vue
@@ -4,25 +4,14 @@
       <div class="card">
         <h1>Opgenomen</h1>
         <ul class="list-group">
-          <li class="list-group-item textleft">
-            <span class="badge badge-primary badge-pill">datum</span>
-            <span class="left">Dapibus ac facilisis in</span>
-            <a class="right actionbuttons"><i class="fa fa-plus fa-2x" aria-hidden="true"></i></a>
-            <a class="right actionbuttons"><i class="fa fa-minus fa-2x" aria-hidden="true"></i></a>
-          </li>
-          <li class="list-group-item textleft">
-            <span class="badge badge-primary badge-pill right">datum</span>
-            <span class="left">Dapibus ac facilisis in</span>
-            <a class="right actionbuttons"><i class="fa fa-plus fa-2x" aria-hidden="true"></i></a>
-            <a class="right actionbuttons"><i class="fa fa-minus fa-2x" aria-hidden="true"></i></a>
-          </li>
-          <li class="list-group-item textleft act">
-            <span class="badge badge-primary badge-pill right">datum</span>
-            <span class="left">Dapibus ac facilisis in</span>
-            <a class="right actionbuttons white"><i class="fa fa-minus fa-2x" aria-hidden="true"></i></a>
+          <li class="list-group-item textleft" v-for="records in records" v-bind:class="{ act: records.actief }">
+            <span class="badge badge-primary badge-pill">{{ records.datum }}</span>
+            <span class="left">{{ records.naam }}</span>
+            <a class="right actionbuttons" @click="toevoegen(records.naam)" v-show="!records.actief"><i class="fa fa-plus fa-2x" aria-hidden="true"></i></a>
+            <a class="right actionbuttons" @click="verwijderen(records.naam)" v-show="records.actief"><i class="fa fa-minus fa-2x" aria-hidden="true"></i></a>
           </li>
         </ul>
-        <nav aria-label="Page navigation example">
+        <nav aria-label="Page navigation example" v-if="records.length >= 5">
           <ul class="pagination justify-content-end">
             <li class="page-item disabled">
               <a class="page-link" href="#" tabindex="-1">Vorige</a>
@@ -36,42 +25,50 @@
           </ul>
         </nav>
       </div>
-      <div class="card">
+      <div class="card" v-show="actief">
         <h1>Afspeellijst</h1>
          <ul class="list-group">
-          <li class="list-group-item textleft act">
-            <span class="badge badge-primary badge-pill right">datum</span>
-            <span class="left">Dapibus ac facilisis in</span>
-            <a class="right actionbuttons white"><i class="fa fa-minus fa-2x" aria-hidden="true"></i></a>
+          <li class="list-group-item textleft act" v-for="actief in actief">
+            <span class="badge badge-primary badge-pill right">{{ actief.datum }}</span>
+            <span class="left">{{ actief.naam }}</span>
+            <a class="right actionbuttons white" @click="verwijderen(actief.naam)"><i class="fa fa-minus fa-2x" aria-hidden="true"></i></a>
           </li>
         </ul>
       </div>
     </div>
-    <p for="data in data">smdl</p>
   </div>
 </template>
 <script>
 /* eslint-disable */
-import Firebase from 'firebase'
+import * as firebase from "firebase";
 export default {
   name: 'hello',
   data () {
     return {
+      records: [],
+      actief: null
     }
   },
   created () {
     this.getlogs();
   },
   methods: {
-    getlogs: function () {
-      let config = {
-          databaseURL: "https://roboplot-1.firebaseio.com/"
-        };  
-      let app = Firebase.initializeApp(config)
-      let db = app.database()
-      let data = db.ref('data')
-      console.log(db)
+    getlogs: function () {  
+      let self = this;
+      firebase.database().ref('data').on('value', function (logs) {
+        self.records = logs.val()
+      });
+      firebase.database().ref('actief').on('value', function (actief) {
+        self.actief = actief.val()
+      })
+    },
+    verwijderen: function(naam){
+      
+    },
+    toevoegen: function(naam){
+      if(this.actief = null){
+      }
     }
-  },
+  }
 }
 </script>
--- vue/roboplot/src/main.js
+++ vue/roboplot/src/main.js
@@ -3,11 +3,19 @@
 import Vue from 'vue'
 import VueFire from 'vuefire'
 import App from './App'
+import * as firebase from 'firebase'
 import 'font-awesome/css/font-awesome.css'
-import '@/assets/js/main.js'
 import '@/assets/sass/main.scss'
 require('../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
 Vue.use(VueFire)
+firebase.initializeApp({
+  apiKey: 'AIzaSyCQlYBkHTw5Q6N6A7_ygs6w8OkrKPE44uY',
+  authDomain: 'roboplot-1.firebaseapp.com',
+  databaseURL: 'https://roboplot-1.firebaseio.com',
+  projectId: 'roboplot-1',
+  storageBucket: 'roboplot-1.appspot.com',
+  messagingSenderId: '775004018292'
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
