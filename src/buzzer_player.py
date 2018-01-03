2795b550ec4560755f4faa6879b20292fb7bcf33
Jef Roosens <jef.roosens@gmail.com>
2018-01-03 16:00:27 +0100

    add sound

--- src/buzzer_player.py
+++ src/buzzer_player.py
@@ -595,6 +595,8 @@ def setup():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(buzzer_pin, GPIO.IN)
 	GPIO.setup(buzzer_pin, GPIO.OUT)
+  GPIO.setup(voltage_pin, GPIO.OUT)
+  pwm = GPIO.PWM(voltage_pin,100)
 	
 def destroy():
 	GPIO.cleanup()				# Release resource
