924817dfd377aa5ffa4992ec9152b3376b0a30a1
Jef Roosens <jefroose@student.arteveldehs.be>
2018-01-09 15:31:26 +0100

    clear folder

--- src/interval.py
+++
@@ -1,9 +0,0 @@
-import threading
-interval = 0.5
-
-def myPeriodicFunction():
-    print("print")
-def startTimer():
-    threading.Timer(interval, startTimer).start()
-    myPeriodicFunction()
-startTimer()
