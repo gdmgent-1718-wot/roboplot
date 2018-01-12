88c65f8f5041abefc1c37cc9e145d3f19c487396
lodemuyl <lodemuyl@student.arteveldehs.be>
2018-01-12 09:31:09 +0100

    heroku update login

--- vue/index.js
+++ vue/index.js
@@ -4,5 +4,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'dist')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('dist/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
